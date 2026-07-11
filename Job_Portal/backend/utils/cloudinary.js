0e2182dbeb1f35cf8f0a431eda57b8a965aedef9
pratham-12-lab <prathamlgaikwad468@gmail.com>
2026-07-11 17:22:46 +0530

    Fix Cloudinary environment variable names

--- Job_Portal/backend/utils/cloudinary.js
+++ Job_Portal/backend/utils/cloudinary.js
@@ -4,9 +4,9 @@ import { v2 as cloudinary } from "cloudinary";
 // by Docker (env_file) or by index.js before this module is used.
 // Configuring here at module load time caused API_KEY to be undefined.
 cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.API_KEY,
-    api_secret: process.env.API_SECRET,
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME || process.env.CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY || process.env.API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET || process.env.API_SECRET,
 });
 
 export default cloudinary;
